d7f63ea10bf36df033e86a88a9e062455dbc6b8c
NomkhosiDube <nomkhosidube2@gmail.com>
2023-11-30 07:20:21 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 from magic_calculation_102 import add, sub
-
-
 def magic_calculation(a, b):
     if a < b:
         c = add(a, b)
@@ -11,4 +9,3 @@ def magic_calculation(a, b):
 
     else:
         return sub(a, b)
-
