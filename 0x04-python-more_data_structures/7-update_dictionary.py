bc4b17da6824c3ee1abe20163f487641775de29b
NomkhosiDube <nomkhosidube2@gmail.com>
2023-12-07 03:18:50 -0800

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 
-
 def update_dictionary(a_dictionary, key, value):
+    """Should replace or add key/value in a dictionary
+    key argument will be always a string
+    value argument will be any type
+    If a key exists in the dictionary, the value will be replaced
+    If a key doesn’t exist in the dictionary, it will be created
     """
-    A function that replaces or adds
-    key/value in a dictionary.
-    """
-    a_dictionary[key] = value
+    # a_dictionary[key] = value # or this
+
+    a_dictionary.update([(key, value)])
+    return a_dictionary
